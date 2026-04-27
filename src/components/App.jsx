f9e4144c1be0822f379b5a5e32bc85c91fdf68f1
Daniel Muthui <dannymuthui118@gmail.com>
2026-04-27 15:55:16 +0300

    feat: updated the app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,54 @@
 import React from "react";
 import blogData from "../data/blog";
 
-console.log(blogData);
+function Header({ name }) {
+  return (
+    <header>
+      <h1>{name}</h1>
+    </header>
+  );
+}
+
+function About({ image, about }) {
+  return (
+    <aside>
+      <img src={image} alt="blog logo" />
+      <p>{about}</p>
+    </aside>
+  );
+}
+
+function Article({ title, date, preview }) {
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date}</small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+function ArticleList({ posts }) {
+  return (
+    <main>
+      {posts.map(post => (
+        <Article
+          key={post.id}
+          title={post.title}
+          date={post.date}
+          preview={post.preview}
+        />
+      ))}
+    </main>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
